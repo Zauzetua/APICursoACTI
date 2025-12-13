7c78b4c714cbaaf2648a696c7463f698095a46af
Zauzetua <yisuschuy12345@hotmail.com>
2025-12-12 18:26:06 -0700

    Quite que el swagger se inicie, es para produccion, no se ocupa

--- APICursoACTI_Web/Program.cs
+++ APICursoACTI_Web/Program.cs
@@ -23,7 +23,7 @@ builder.Services.AddSingleton<ITaskItemRepository, InMemoryTaskItemRepository>()
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment() || app.Environment.IsProduction())
+if (app.Environment.IsDevelopment())
 {
     app.UseSwagger();
     app.UseSwaggerUI(c =>
